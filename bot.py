35e74ed5c7aa82834f1470921ff6cb0994fc0a0f
Tripdevvv <naomkers02@gmail.com>
2025-02-04 05:33:12 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Update
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")  # Токен бота (добавим в Render)
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # URL, который выдаст Render
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    await bot.set_webhook(WEBHOOK_URL)
+
+@app.post("/")
+async def receive_update(request: Request):
+    data = await request.json()
+    update = Update(**data)
+    await dp.feed_update(bot, update)
+    return {"ok": True}
+
+@dp.message()
+async def echo_handler(message: types.Message):
+    await message.answer(f"Вы сказали: {message.text}")
