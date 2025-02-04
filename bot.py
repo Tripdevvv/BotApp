1236ec954f90e390c8f0ddf1c2c9e48b61283eef
Tripdevvv <naomkers02@gmail.com>
2025-02-04 07:53:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
+import logging
 from telegram import Update, Bot
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, Dispatcher
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import os
 
 TOKEN = '8068604657:AAFUkVGe3OMcs8WTiqNaoKcQm9sAU3_OTDQ'  # Вставьте ваш токен
 bot = Bot(TOKEN)
 
 # Функция для обработки команды /start
-async def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id  # Получаем Telegram ID
     user_name = update.message.from_user.first_name  # Имя пользователя
 
@@ -16,7 +18,7 @@ async def start(update: Update, context: CallbackContext) -> None:
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
-    await update.message.reply_text(
+    update.message.reply_text(
         f"Привет, {user_name}!\nЯ могу помочь тебе с расчетом сроков годности продуктов.",
         reply_markup=reply_markup
     )
@@ -30,22 +32,29 @@ async def start(update: Update, context: CallbackContext) -> None:
         f.write(f"{user_id}\n")
 
 # Функция для отправки данных с сайта в Telegram
-async def send_to_telegram(telegram_id, product_name, shelf_life, start_date, end_date):
+def send_to_telegram(telegram_id, product_name, shelf_life, start_date, end_date):
     message = f"Продукт: {product_name}\n"
     message += f"Срок годности: {shelf_life} дней\n"
     message += f"Начальный срок: {start_date}\n"
     message += f"Конечный срок: {end_date}"
-    await bot.send_message(chat_id=telegram_id, text=message)
+    bot.send_message(chat_id=telegram_id, text=message)
 
-# Основная функция для запуска бота
 def main() -> None:
-    application = Application.builder().token(TOKEN).build()
+    # Получаем порт из переменной окружения
+    port = int(os.getenv("PORT", 5000))  # Установим дефолтный порт на 5000, если Render не предоставляет порт
+
+    # Создаем объект Updater с токеном
+    updater = Updater(TOKEN)
+
+    # Получаем диспетчер для регистрации обработчиков
+    dispatcher = updater.dispatcher
 
     # Добавляем обработчик для команды /start
-    application.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("start", start))
 
     # Запускаем бота
-    application.run_polling()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
