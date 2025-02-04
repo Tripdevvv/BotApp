5cd63ed11b53c0fa73943a00bc6180c0e71657e6
Tripdevvv <naomkers02@gmail.com>
2025-02-04 08:22:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,10 +40,11 @@ async def main() -> None:
     # Добавляем обработчик для команды /start
     application.add_handler(CommandHandler("start", start))
 
-    # Запускаем бота (без использования asyncio.run)
+    # Запускаем бота (Render уже управляет event loop)
     await application.run_polling()
 
 if __name__ == '__main__':
     import asyncio
     # Запуск с использованием run_polling() без asyncio.run()
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.ensure_future(main())
+    asyncio.get_event_loop().run_forever()
